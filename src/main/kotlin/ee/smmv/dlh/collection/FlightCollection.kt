c32b999337db100ee96611694903d734164e0257
Josef Petrak <josef.petrak@somemove.cz>
2018-11-17 19:18:42 +0100

    Package for collection entities

--- src/main/kotlin/ee/smmv/dlh/collection/FlightCollection.kt
+++ src/main/kotlin/ee/smmv/dlh/collection/FlightCollection.kt
@@ -1,6 +1,7 @@
-package ee.smmv.dlh.model
+package ee.smmv.dlh.collection
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import ee.smmv.dlh.model.Flight
 
 data class FlightCollection(
 	@get:JsonProperty("Flight") var flights: List<Flight> = listOf()
--- src/main/kotlin/ee/smmv/dlh/resource/FlightStatusResource.kt
+++ src/main/kotlin/ee/smmv/dlh/resource/FlightStatusResource.kt
@@ -1,7 +1,7 @@
 package ee.smmv.dlh.resource
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import ee.smmv.dlh.model.FlightCollection
+import ee.smmv.dlh.collection.FlightCollection
 
 @JsonIgnoreProperties(*["Meta"])
 data class FlightStatusResource(
